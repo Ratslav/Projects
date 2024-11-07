956d48e7febccb07e0776df2c68b104487304790
Pavel Ratslav <pasha06051996@gmail.com>
2024-11-07 15:46:22 +0300

    3

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -5,3 +5,17 @@ class Board:
     def make_move(self, row, col, player):
         self.board[row][col] = player
     
+    def display(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
+
+# Создать игровое поле - объект класса Board.
+game = Board()
+# Отрисовать поле в терминале.
+game.display()
+# Разместить на поле символ по указанным координатам - сделать ход.
+game.make_move(1, 1, 'X')
+print('Ход сделан!')
+# Перерисовать поле с учётом сделанного хода.
+game.display() 

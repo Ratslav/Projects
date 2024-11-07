2a127bcb92a63595d3ee1577057be6a738d42861
Pavel Ratslav <pasha06051996@gmail.com>
2024-11-07 15:36:29 +0300

    1

---
+++ tic_tac_toe/game.py
@@ -0,0 +1,7 @@
+class Board:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+    
+    def make_move(self, row, col, player):
+        self.board[row][col] = player
+        

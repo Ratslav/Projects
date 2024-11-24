aaf692127165a4b57e39be7f228e112adcedb1fa
Pavel Ratslav <pasha06051996@gmail.com>
2024-11-24 15:32:46 +0300

    home

--- test/test.py
+++ test/test.py
@@ -1,10 +1,24 @@
 def main(arr):
+    schet = 0
+    result = 0
+    stroka = ''
+    for i1 in range(0,len(arr)):
+        if arr[i1] not in stroka:
+            stroka+=arr[i1]
+            schet+=1
+            continue
+        if schet > result:
+            result = schet
+        stroka = ''
+        schet = 0
+    return result
+    
 
-    return None
+
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as file_in:
-        arr = tuple(file_in.readline().split())
+        arr = str(file_in.readline().rstrip('\n').split(' '))
         #n = int(file_in.readline())
     result = main(arr)
 
--- test/test_2.py
+++ test/test_2.py
@@ -1,4 +1,15 @@
-a = (1, 2, 3, 4)
-b=[]
-b[1] = 2
-print(a[0])
+def find_two_indexes(data, expected_sum):
+    # Ваше решение?
+    for i1 in range(0,len(data)):
+        for i2 in range(0, len(data)):
+            if i2==i1:
+                i2+=1
+                continue
+            if data[i2]+data[i1]==expected_sum:
+                return (i1, i2)
+
+
+if __name__ == '__main__':
+    data = [1, 2, 3, 4, 5, 6, 7, 11]
+    expected_sum = 10
+    print(find_two_indexes(data, expected_sum))
